621d1b1b99ecf065e437a5942960a1889a095f78
DAW <daw@daw-VirtualBox.(none)>
2014-03-14 17:08:25 +0100

    Ejercicios jQuery corregidos

--- jQuery/js/tabs.js
+++ jQuery/js/tabs.js
@@ -3,26 +3,35 @@ $(function(){
 
     //Ocultar todos los elementos div.module;
     var $modulos = $('div.module');
-
     $modulos.hide();
 
     //Crear una lista desordenada antes del primer div.module para utilizar como pestañas;
     var $new = $('<ul/>');
-    $new.insertBefore($modulos[0]);
+    $new.insertBefore($modulos.first());
 
+    var $lis =[];
     //Interactuar con cada div utilizando $.fn.each. Por cada uno, utilizar el texto del elemento h2 como el texto para el ítem de la lista desordenada;
     $modulos.each(function(){
-        $nombre = $(this).find('h2').text();
-        console.log($nombre);
-        $('<li>' + $nombre + '</li>').appendTo($new);
+        var $div = $(this);
+        var $nombre = $div.find('h2').text();
+        var $li = $('<li/>', {
+            'text' : $nombre
+        });
+
+        $li.data('div', $div);
+
+        $lis.push($li.get(0));
     });
+
+    $new.append($lis);
 
     //Vincular un evento click a cada ítem de la lista de forma que
     //Muestre el div correspondiente y oculte el otro;
     //Añada la clase "current" al ítem seleccionado;
     //Remueva la clase "current" del otro ítem de la lista;:
-    $new.delegate('li','click', function(){
-
+    $new.on('click','li', function(){
+        var $li = $(this);
+        var $div = $li.data('div');
     });
 
 });
